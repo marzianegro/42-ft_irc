00ea6f5a420d6e46fa4e37d1c92205e3647b9684
marzianegro <ngr.marzia@gmail.com>
2024-02-27 19:35:11 +0100

    working on looped demo

--- mnegro-demo/client.cpp
+++ mnegro-demo/client.cpp
@@ -6,7 +6,7 @@
 /*   By: mnegro <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:18:55 by mnegro            #+#    #+#             */
-/*   Updated: 2024/02/27 18:22:16 by mnegro           ###   ########.fr       */
+/*   Updated: 2024/02/27 19:34:55 by mnegro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,56 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-// client stages
-int	main(void) {
+// // client stages (single)
+// int	main(void) {
 
+// 	// 1. creating the client socket
+// 	int	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+// 	if (clientSocket == -1) {
+// 		std::cout << "An error occured during client socket creation\n";
+// 		exit(EXIT_FAILURE);
+// 	} else {
+// 		std::cout << "Client socket created\n";
+// 	}
+
+// 	// 2. defining server address
+// 	sockaddr_in	serverAddress;
+// 	serverAddress.sin_family = AF_INET;
+// 	serverAddress.sin_port = htons(8080);
+// 	serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+// 	// 3. connecting to the server
+// 	if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+// 		std::cout << std::strerror(errno) << '\n';
+// 		exit(EXIT_FAILURE);
+// 	} else {
+// 		std::cout << "Client socket connected to server\n";
+// 	}
+
+// 	// 4. sending data to server
+// 	const char	*message = "Hello, server!\n";
+// 	ssize_t	bytesSent = send(clientSocket, message, strlen(message), 0);
+// 	if (bytesSent == -1) {
+// 		std::cout << "An error occured while sending message to server\n";
+// 		exit (EXIT_FAILURE);
+// 	} else {
+// 		std::cout << "Message sent to server: " << message;
+// 	}
+
+// 	// 5. closing the client socket
+// 	close(clientSocket);
+
+// 	return (0);
+// }
+
+// // client stages (loop)
+int	main(int ac, char **av) {
+	if (ac != 2) {
+		std::cout << "Missing message for server\n";
+		exit(EXIT_FAILURE);
+	}
 	// 1. creating the client socket
-	int	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	int	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == -1) {
 		std::cout << "An error occured during client socket creation\n";
 		exit(EXIT_FAILURE);
@@ -44,13 +89,12 @@ int	main(void) {
 	}
 
 	// 4. sending data to server
-	const char	*message = "Hello, server!\n";
-	ssize_t	bytesSent = send(clientSocket, message, strlen(message), 0);
+	ssize_t	bytesSent = send(clientSocket, av[1], strlen(av[1]), 0);
 	if (bytesSent == -1) {
 		std::cout << "An error occured while sending message to server\n";
 		exit (EXIT_FAILURE);
 	} else {
-		std::cout << "Message sent to server: " << message;
+		std::cout << "Message sent to server: " << av[1] << '\n';
 	}
 
 	// 5. closing the client socket
--- mnegro-demo/server.cpp
+++ mnegro-demo/server.cpp
@@ -6,7 +6,7 @@
 /*   By: mnegro <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:19:01 by mnegro            #+#    #+#             */
-/*   Updated: 2024/02/27 18:20:57 by mnegro           ###   ########.fr       */
+/*   Updated: 2024/02/27 19:34:57 by mnegro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,78 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-// server stages
+// // server stages (single)
+// int	main(void) {
+
+// 	// 1. creating the server socket
+// 	int	serverSocket = socket(PF_INET, SOCK_STREAM, 0); // AF_INET
+// 	if (serverSocket == -1) {
+// 		std::cout << "An error occured during server socket creation\n";
+// 		exit(EXIT_FAILURE);
+// 	} else {
+// 		std::cout << "Server socket created\n";
+// 	}
+
+// 	// 2. defining server address
+// 	sockaddr_in	serverAddress;
+// 	serverAddress.sin_family = AF_INET;
+// 	serverAddress.sin_port = htons(8080);
+// 	// option 1 is to bind the socket to a particular IP
+// 	// const char	*ipAddress = "192.168.0.1";
+// 	// if (inet_pton(AF_INET, ipAddress, &(serverAddress.sin_addr) <= 0) {
+// 	// 	std::cout << "Invalid IP address format OR Conversion error\n";
+// 	// 	exit (EXIT_FAILURE);
+// 	// }
+// 	// option 2 is to make the socket bind and listen to all the available IPs
+// 	serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+// 	// 3. binding the server socket
+// 	if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+// 		std::cout << std::strerror(errno) << '\n';
+// 		exit(EXIT_FAILURE);
+// 	} else {
+// 		std::cout << "Server socket bound\n";
+// 	}
+
+// 	// 4. listening for connections
+// 	if (listen(serverSocket, 5) == -1) {
+// 		std::cout << std::strerror(errno) << '\n';
+// 		exit(EXIT_FAILURE);
+// 	} else {
+// 		std::cout << "Server socket now listening\n";
+// 	}
+
+// 	// 5. accepting a client connection
+// 	int	clientSocket = accept(serverSocket, NULL, NULL);
+// 	if (clientSocket == -1) {
+// 		std::cout << "An error occured during client socket creation\n";
+// 		exit(EXIT_FAILURE);
+// 	} else {
+// 		std::cout << "Client socket accepted\n";
+// 	}
+
+// 	// 6. receiving data from the client
+// 	char	buffer[1024] = {0};
+// 	ssize_t	bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+// 	if (bytesRead == -1) {
+// 		std::cout << "An error occuring while receiving data from client\n";
+// 	} else if (bytesRead == 0) {
+// 		std::cout << "Connection closed by remote side\n";
+// 	} else {
+// 		std::cout << "Message received from client: " << buffer;
+// 	}
+
+// 	// 7. closing the server socket
+// 	close(serverSocket);
+
+// 	return (0);
+// }
+
+//// server stages (loop)
 int	main(void) {
 
 	// 1. creating the server socket
-	int	serverSocket = socket(PF_INET, SOCK_STREAM, 0); // AF_INET
+	int	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (serverSocket == -1) {
 		std::cout << "An error occured during server socket creation\n";
 		exit(EXIT_FAILURE);
@@ -45,37 +112,48 @@ int	main(void) {
 	// 3. binding the server socket
 	if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
 		std::cout << std::strerror(errno) << '\n';
+		close(serverSocket);
 		exit(EXIT_FAILURE);
 	} else {
 		std::cout << "Server socket bound\n";
 	}
 
 	// 4. listening for connections
-	if (listen(serverSocket, 5) == -1) {
+	if (listen(serverSocket, 128) == -1) {
 		std::cout << std::strerror(errno) << '\n';
+		close (serverSocket);
 		exit(EXIT_FAILURE);
 	} else {
 		std::cout << "Server socket now listening\n";
 	}
 
 	// 5. accepting a client connection
-	int	clientSocket = accept(serverSocket, NULL, NULL);
-	if (clientSocket == -1) {
-		std::cout << "An error occured during client socket creation\n";
-		exit(EXIT_FAILURE);
-	} else {
-		std::cout << "Client socket accepted\n";
-	}
+	while (true) {
+		sockaddr_in	clientAddress;
+		socklen_t	clientAddressLen = sizeof(clientAddress);
 
-	// 6. receiving data from the client
-	char	buffer[1024] = {0};
-	ssize_t	bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-	if (bytesRead == -1) {
-		std::cout << "An error occuring while receiving data from client\n";
-	} else if (bytesRead == 0) {
-		std::cout << "Connection closed by remote side\n";
-	} else {
-		std::cout << "Message received from client: " << buffer;
+		int	clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLen);
+		if (clientSocket == -1) {
+			std::cout << "An error occured during client socket creation\n";
+			close(clientSocket);
+			continue ; // skip to the next iteration of the loop
+		} else {
+			std::cout << "Client socket accepted\n";
+		}
+
+		// 6. receiving data from the client
+		char	buffer[1024];
+		ssize_t	bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+		if (bytesRead == -1) {
+			std::cout << "An error occuring while receiving data from client\n";
+		} else if (bytesRead == 0) {
+			std::cout << "Connection closed by remote side\n";
+		} else {
+			buffer[bytesRead] = '\0';
+			std::cout << "Message received from client: " << buffer;
+		}
+
+		close(clientSocket);
 	}
 
 	// 7. closing the server socket
